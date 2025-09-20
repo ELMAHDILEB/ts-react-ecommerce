727686b2c1644b2fca0266dfbf5cc13f2dfd353d
ELMAHDILEB <elmahdibelcadi@gmail.com>
2025-09-20 23:04:59 +0000

    fix pagination display when no results found

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import { useSearchParams } from "react-router-dom";
 import Header from "./Components/Header";
 import Products from "./Components/Products";
 import { useGetProductsQuery } from "./api/apiSlice";
+// import type { Product } from "./Types/Product";
 
 const App = () => {
 
   const [searchParams, setSearchParams] = useSearchParams();
   const page = Number(searchParams.get("_page")) || 1;
   const limit = Number(searchParams.get("_limit")) || 10;
-  const { data: products=[], error, isLoading} = useGetProductsQuery({page, limit});
+  const { data, error, isLoading} = useGetProductsQuery({page, limit});
+  const products = data?.products ?? [];
 
 
   if(isLoading) return <h1 className="w-full h-screen text-3xl font-bold flex items-center justify-center">isLoading...</h1>;
@@ -17,7 +19,7 @@ const App = () => {
   return (
     <div className=" w-full md:w-[80%]   mx-auto p-3 ">
       <Header />
-        <Products items={products}  page={page} limit={limit} setSearchParams={setSearchParams}/>
+        <Products items={products}  page={page} limit={limit} setSearchParams={setSearchParams} total={data?.total ?? 0}/>
     </div>
   );
 };
--- src/api/apiSlice.ts
+++ src/api/apiSlice.ts
@@ -1,19 +1,22 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import type { Product } from "../Types/Product";
-
+interface ProductsResponse {
+    products: Product[];
+    total: number;
+  }
 
 
 export const  apiSlice  = createApi({
     reducerPath:  "api", // name slice on store
     baseQuery: fetchBaseQuery({baseUrl: "https://dummyjson.com/"}),
     endpoints: (builder)=>({
-     getProducts: builder.query<{
-        products: Product[]; total: number}, // resposne type
+     getProducts: builder.query<ProductsResponse, 
         {page?:number; limit?:number} // argument type
         >({ 
-        query: ({page = 1, limit = 20} = {} )=> `/products?limit=${limit}&skip=${(page - 1) * limit}`,
+        query: ({page = 1, limit = 10})=> `/products?limit=${limit}&skip=${(page - 1) * limit}`,
      })
     })
 })
 
 export const { useGetProductsQuery } = apiSlice;
+
